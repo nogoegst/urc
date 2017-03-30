d4f81e67b0b06aa76e5d01c8e2ab22d3440b41d7
Ivan Markin <twim@riseup.net>
2017-03-30 11:52:15 +0000

    Don't use battery on non-OpenBSD

--- battery_other.go
+++ battery_other.go
@@ -1,17 +1,14 @@
-// battery.go - battery status updater.
+// battery_other.go - fake battery status updater.
 //
 // To the extent possible under law, Ivan Markin waived all copyright
 // and related or neighboring rights to this module of urc, using the creative
 // commons "cc0" public domain dedication. See LICENSE or
 // <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
 
+// +build !openbsd
+
 package main
 
-import (
-	"time"
-)
-
-type batteryLifetime struct {
-	Percent int
-	Time    time.Duration
+func batteryCheck(batteryCh chan<- batteryLifetime) {
+	batteryCh <- batteryLifetime{Percent: -1}
 }
